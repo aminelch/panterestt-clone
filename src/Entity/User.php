893ee95eeb133e3c4acca70d52d6d15b97b544a1
aminelch <aminelch95@gmail.com>
2021-07-22 19:50:18 +0100

    🔧 rename user table to users table and generate a migration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table("users")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
