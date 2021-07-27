75ab21137e9ff090199c0d17caf9040294179c7f
aminelch <aminelch95@gmail.com>
2021-07-27 14:21:31 +0100

    🔨 use POST method instead GET to logout a user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,10 +27,11 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="app_logout", methods="POST")
      */
     public function logout()
     {
+
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
